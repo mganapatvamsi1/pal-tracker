5eca3de455214956bb20c5656178a7b38cc50b47
Manikanta Ganapathiraju <ganapathiraju.vamsi@gmail.com>
2018-01-17 15:52:10 -0500

    add use of environment variables

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,12 +2,20 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
-public class WelcomeController {
+    public class WelcomeController {
+
+    private String hello;
+
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message) {
+            hello = message;
+        }
 
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return hello;
     }
 }
